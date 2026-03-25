17e58c68a39c7236eeb8e7011b12cde3d1ebe3da
Eduardo <eduardooliveira7907@gmail.com>
2026-03-25 08:56:42 -0300

    setup inicial do projeto product-api

---
+++ user-api/src/main/java/com/santana/java/back/end/user_api/config/FlywayConfig.java
@@ -0,0 +1,29 @@
+package com.santana.java.back.end.user_api.config;
+
+import org.flywaydb.core.Flyway;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FlywayConfig {
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String user;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Bean(initMethod = "migrate")
+    public Flyway flyway() {
+        return Flyway.configure()
+                .dataSource(url, user, password)
+                .locations("classpath:db/migration")
+                .baselineOnMigrate(true)
+                .loggers("debug")  // ou "info"
+                .load();
+    }
+}
