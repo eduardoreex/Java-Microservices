40e2586b4f6a4676dbd8d4f349698c7d92e6609e
Eduardo <eduardooliveira7907@gmail.com>
2026-04-14 09:33:33 -0300

    Ajustando o Shop e definindo criação no banco

--- shopping-api/src/main/java/com/santana/java/back/end/shopping_api/model/Shop.java
+++ shopping-api/src/main/java/com/santana/java/back/end/shopping_api/model/Shop.java
@@ -6,35 +6,22 @@ import jakarta.persistence.*;
 import java.util.Date;
 import java.util.List;
 
-// @Entity avisa: "Esta classe é o espelho exato da tabela 'shop' no PostgreSQL!"
 @Entity(name="shop")
 public class Shop {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    // Código do usuário que comprou
     private  String userIdentifier;
-    // Valor total
     private  float total;
-    // Data exata da compra
     private Date data;
 
-    // 🚨 A LISTA DE ITENS (A MÁGICA DA ORQUESTRAÇÃO) 🚨
-    // Lembra do estacionamento? A 'List<Item>' é um estacionamento que guarda vários itens dentro desta única compra!
 
-    // @ElementCollection avisa: "Java, isto não é um dado simples (como texto). É uma coleção de filhos (@Embeddable)."
-    // fetch = FetchType.EAGER diz: "Sempre que você for no banco buscar a Compra, traga os Itens dela JUNTOS e imediatamente."
     @ElementCollection(fetch = FetchType.EAGER)
 
-    // @CollectionTable ensina o Java ONDE estão os itens no banco:
-    // name = "item" -> "Estão na tabela chamada 'item'"
-    // joinColumns = @JoinColumn(name = "shop_id") -> "A corda que amarra o item à compra é a coluna 'shop_id'"
+
     @CollectionTable(name = "item", joinColumns = @JoinColumn(name = "shop_id"))
 
     private List<Item> items;
-
-
-    // --- GETTERS E SETTERS (As portas de acesso) ---
 
     public long getId() {
         return id;
@@ -45,7 +32,6 @@ public class Shop {
     public float getTotal() {
         return total;
     }
-    // Devolve o estacionamento inteiro de itens
     public List<Item> getItems() {
         return items;
     }
@@ -61,7 +47,6 @@ public class Shop {
     public void setData(Date data) {
         this.data = data;
     }
-    // Grava um estacionamento inteiro de itens novo
     public void setItems(List<Item> items) {
         this.items = items;
     }
--- shopping-api/src/main/resources/db/migration/V1__create_shop_table.sql
+++ shopping-api/src/main/resources/db/migration/V1__create_shop_table.sql
@@ -1,6 +1,5 @@
 create schema if not exists shopping;
 
--- Tabela principal da Compra
 create table shopping.shop (
     id bigserial primary key,
     user_identifier varchar(100) not null,
@@ -8,7 +7,6 @@ create table shopping.shop (
     total float not null
 );
 
--- Tabela de Itens da Compra (Cada linha é um produto comprado)
 create table shopping.item (
     shop_id bigint references shopping.shop(id),
     product_identifier varchar(100) not null,
